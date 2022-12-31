41f4343dfb728b37cfdb3667bea7cb462f66d5dc
Bryn Dole <bryn.dole@gmail.com>
2022-12-30 18:20:11 -0800

    slow down for testing

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
@@ -100,8 +100,8 @@ public final class DrivetrainConstants {
 
   public static final int TIMEOUT_MS = 30;
 
-  public static final double AUTO_MAX_SPEED_METERS_PER_SECOND = 3;
-  public static final double AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 3;
+  public static final double AUTO_MAX_SPEED_METERS_PER_SECOND = 2.0;
+  public static final double AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 2.0;
   public static final double AUTO_MAX_ANGULAR_SPEED_RADIANS_PER_SECOND = 2.0 * Math.PI;
   public static final double AUTO_MAX_ANGULAR_SPEED_RADIANS_PER_SECOND_SQUARED = 2.0 * Math.PI;
 
